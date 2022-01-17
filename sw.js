83b9d9401804ed97c40647d8344cd6f0f0837c42
jsptt <31617952+jspgg@users.noreply.github.com>
2022-01-17 11:02:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/bundle.5be5627b.js')
+importScripts(__FILE__='https://jspgg.github.io/assets/bundle.5be5627b.js')
+//
+//https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/bundle.5be5627b.js
